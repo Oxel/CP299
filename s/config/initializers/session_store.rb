e9baa0f031649b1faee79939eea0e2aa31e59722
vagrant <vagrant@rails-dev-box>
2014-08-29 13:54:05 +0000

    Uploading images

---
+++ s/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_s_session',
+  :secret      => '36851346f2fe74f7eb89387c9a881ecebd8978e6ac866b789300b884cbe2facf84d77e9b57842d48743e6159e769beeec16e1d8754581d97c5505334b54e368f'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
