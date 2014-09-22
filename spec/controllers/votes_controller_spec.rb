122b6a332c6bb341f21afecbdf224ba5af69c87a
Louis Morin <louismorin99@gmail.com>
2014-09-22 13:58:39 -0400

    votedown controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -7,15 +7,32 @@ describe VotesController do
 
 	describe '#upvote' do
 		it "adds an up-vote to the post" do
-			request.env["HTTP_REFERER"] = '/'
 			@user = authenticated_user
 			@post = associated_post
+			request.env["HTTP_REFERER"] = '/'
 			sign_in @user
 
 			expect {
 				post( :up_vote, post_id: @post.id )
 			}.to change{ @post.up_votes }.by 1
-		end
+	end
+	
+	describe '#downvote' do
+		it "adds an down-vote to the post" do
+			@user = authenticated_user
+			@post = associated_post
+			request.env["HTTP_REFERER"] = '/'
+			sign_in @user
+
+			expect {
+				post( :down_vote, post_id: @post.id )
+			}.to change{ @post.up_votes }.by -1
+	end
+
+
+
+
+
 	end
 
 end
