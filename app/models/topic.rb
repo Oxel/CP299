54f400cda77dc8de4fa7b290c71a6309fd59ad5a
Louis Morin <louismorin99@gmail.com>
2014-09-23 13:28:52 -0400

    Another Interlude

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, :autosave => true
 
-  scope :visible_to, -> (user) { user ? all: where(public: true) && all: where(private: true) }
+  scope :visible_to, -> (user) { user ? all: where(public: true) }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,12 +57,12 @@ ActiveRecord::Schema.define(version: 20140922184844) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20140922184844) do
     t.string   "role"
     t.string   "avatar"
     t.string   "Image"
-    t.boolean  "email_favorites"
+    t.boolean  "email_favorites",        default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
