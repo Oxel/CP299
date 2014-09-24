4e36a5988623342e646cee0d3cf7c6f19d4af844
Louis Morin <louismorin99@gmail.com>
2014-09-24 11:07:16 -0400

    Popular Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,6 +28,7 @@ class Post < ActiveRecord::Base
   end
 
   default_scope { order('rank DESC') }
+    scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
   scope :ordered_by_title, -> { order(:title) }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
   
